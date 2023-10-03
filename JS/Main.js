fb12808fe133d816069b60f3c9e1dbf5e5bf9566
BarucBM <baruc.bm@hotmail.com>
2023-10-03 18:30:49 -0300

    Botões adicionados

--- JS/Main.js
+++ JS/Main.js
@@ -1,5 +1,6 @@
 const pokemonol = document.getElementById('PokemonsList')
 const loadMoreButton = document.getElementById('LoadMore')
+const StatsBT = document.getElementsByClassName('StatsBT')
 const maxPoke = 151
 const limit = 6
 let offset = 0
@@ -8,21 +9,21 @@ let offset = 0
 function loadPokemonItems(offset, limit){
     pokeapi.getPokemons(offset, limit).then((pokemonlist = []) => {
         const NewHTML = pokemonlist.map((pokemon) => 
-        `<li class="Pokemon ${pokemon.type}">
-                    <span class="Number">#0${pokemon.number}</span>
-                    <span class="Name">${pokemon.name}</span>
-                    <div class="Detail">
-                        <ol class="Types">
-                            ${pokemon.types.map((type) => `<li class="Type ${type}">${type}</li>`).join('')}
-                        </ol>
-                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.number}.png  " 
-                        alt="${pokemon.name}">
-                    </div>
-                </li>`).join('')
+        `<button class="StatsBT" onClick="Click(this.value);" value="${pokemon.number}"> 
+        <li class="Pokemon ${pokemon.type}">
+            <span class="Number">#${pokemon.number}</span>
+            <span class="Name">${pokemon.name}</span>
+               <div class="Detail">
+                    <ol class="Types">
+                        ${pokemon.types.map((type) => `<li class="Type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.number}.png  " 
+                    alt="${pokemon.name}">
+                </div>
+        </li>
+        </button>`).join('')
         pokemonol.innerHTML += NewHTML
-
-    })  
-}
+})}
 
 loadPokemonItems(offset, limit)
 
@@ -34,19 +35,16 @@ loadMoreButton.addEventListener('click', () => {
     if (qntPoke >= maxPoke){
         const limit = maxPoke - offset
         loadPokemonItems(offset, limit)
-
         loadMoreButton.parentElement.removeChild(loadMoreButton)
     }else{
         loadPokemonItems(offset, limit)
-
     }
-
-    
-
 })
 
+function Click(valor){
+    console.log(valor)
 
-                        
+}
 
 
 
