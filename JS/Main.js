71d03b888db41c0023e986a07decf09c08aeb281
Baruc Moreira <121053838+BarucBM@users.noreply.github.com>
2023-10-27 10:07:08 -0300

    Stats Bars

--- JS/Main.js
+++ JS/Main.js
@@ -9,6 +9,7 @@ let NroPoke = 0
 
 
 
+
 function loadPokemonItems(offset, limit){
     pokeapi.getPokemons(offset, limit).then((pokemonlist = []) => {
         const NewHTML = pokemonlist.map((pokemon) => 
@@ -51,8 +52,19 @@ function Status(NroPoke, limit){
 
 
     pokeapi.getPokemons(NroPoke, limit).then((pokemonlist = []) => {
-        const NewHTML = pokemonlist.map((pokemon) => 
-        `
+
+        const NewHTML = pokemonlist.map(function(pokemon) { 
+            var root = document.documentElement;
+            root.style.setProperty('--Valor-vida', pokemon.hp),
+            root.style.setProperty('--Valor-ataque', pokemon.attack), 
+            root.style.setProperty('--Valor-defesa', pokemon.defense), 
+            root.style.setProperty('--Valor-ataquesp', pokemon.specialAttack), 
+            root.style.setProperty('--Valor-defesasp', pokemon.specialDefense),
+            root.style.setProperty('--Valor-velocidade', pokemon.speed),
+            console.log(pokemon.hp)
+
+
+            return((`        
         <section class="content ${pokemon.type}">
             <div class="IMG ${pokemon.type}">
                 
@@ -67,43 +79,86 @@ function Status(NroPoke, limit){
                 <h2 class= "Tipos">
                 ${pokemon.types.map((type) => `<li class="Type ${type}">${type}</li>`).join('')}
                 </h2>
-                </div>
+            </div>
     
             <div class="Status">
-            <span>
-                Características
+            <div class="Info">
+                <span>
+                    Características
+                    
+                </span>
+
+                <p>
+                    Vida
+                </p>
+                <p>
+                    Ataque
+                </p>
+                <p>
+                    Defesa
+                </p>
+                <p>
+                    Ataque especial
+                </p>
+                <p>
+                    Defesa especial
+                </p>
+                <p>
+                    Velocidade
+                </p>
+                </div>
+
+
+        <div class="Valores">
+                <br>
+                <br>
                 
-            </span>
-            <p>
-                Vida ${pokemon.hp}
-            </p>
-            <p>
-                Ataque ${pokemon.attack}
-            </p>
-            <p>
-                Defesa ${pokemon.defense}
-            </p>
-            <p>
-                Ataque especial ${pokemon.specialAttack}
-            </p>
-            <p>
-                Defesa especial ${pokemon.specialDefense}
-            </p>
-            <p>
-                Velocidade ${pokemon.speed}
-            </p>
+            <div class="one">  
+                <p>
+                    ${pokemon.hp}<div class="progress" ><div class="vida"></div></div>
+                </p>
+            </div>
+            <div class="one">  
+                <p>
+                    ${pokemon.attack}<div class="progress" ><div class="ataque"></div></div>
+                </p>
+            </div>
+            <div class="one">  
+                <p>
+                    ${pokemon.defense}<div class="progress" ><div class="defesa"></div></div>
+                </p>
+            </div>
+            <div class="one">  
+                <p>
+                    ${pokemon.specialAttack}<div class="progress" ><div class="ataquesp"></div></div>
+                </p>
+            </div>
+            <div class="one">  
+                <p>
+                    ${pokemon.specialDefense}<div class="progress" ><div class="defesasp"></div></div>
+                </p>
+            </div>
+            <div class="one">  
+                <p>
+                    ${pokemon.speed}<div class="progress" ><div class="velocidade"></div></div>
+                </p>
+            </div>
 
         </div>
-    
-    
-    
-    
-    
-        </section>`).join('')
-       Corpo.innerHTML += NewHTML
-       console.log (pokemonlist)
-})
+        </div>
 
+    
+    
+    
+        </section>`
+
+        ))}
+
+ 
+        )
+        Corpo.innerHTML += NewHTML.join('')
+    })
+    
 }
 
 
